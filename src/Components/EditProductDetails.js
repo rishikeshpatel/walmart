fe985a7a00d043781955b6262172ccf786912b99
Rishikesh Shailendra <34436@ITCINFOTECH.COM>
2020-08-13 13:42:12 +0530

    added image to edit page

--- src/Components/EditProductDetails.js
+++ src/Components/EditProductDetails.js
@@ -29,65 +29,69 @@ class EditProductDetails extends Component {
           <DialogTitle id="form-dialog-title">
             Update Product Details
           </DialogTitle>
-          <DialogContent>
-            {/* <div className="product-image-container">
-                <img
-                  src={
-                    "https://mobile-tha-server-8ba57.firebaseapp.com/" +
-                    data.productImage
-                  }
-                  alt=""
-                ></img>
-              </div> */}
-            <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              label="Product Name"
-              type="text"
-              value={data.productName}
-              fullWidth
-            />
-            <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              label="Review Rating"
-              type="number"
-              value={data.reviewRating}
-              fullWidth
-            />
-            <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              label="Long Description"
-              type="test"
-              value={data.longDescription}
-              multiline
-              fullWidth
-            />
-            <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              label="Short Description"
-              type="textarea"
-              multiline
-              value={data.shortDescription}
-              fullWidth
-            />
-            <FormLabel component="legend">In Stock</FormLabel>
-            <RadioGroup
-              className="inStock"
-              aria-label="instock"
-              name="instock"
-              value={1}
-            >
-              <FormControlLabel value="No" control={<Radio />} label="No" />
-              <FormControlLabel value="yes" control={<Radio />} label="yes" />
-            </RadioGroup>
-          </DialogContent>
+          <div style={{display: 'flex'}}>
+            <div className="edit-page-image-container">
+              <img
+                src={
+                  "https://mobile-tha-server-8ba57.firebaseapp.com/" +
+                  data.productImage
+                }
+                alt=""
+                style={{width:'250px', height:'250px'}}
+              ></img>
+            </div>
+            <DialogContent>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Product Name"
+                type="text"
+                value={data.productName}
+                fullWidth
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Review Rating"
+                type="number"
+                value={data.reviewRating}
+                fullWidth
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Long Description"
+                type="test"
+                value={data.longDescription}
+                multiline
+                fullWidth
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Short Description"
+                type="textarea"
+                multiline
+                value={data.shortDescription}
+                fullWidth
+              />
+              <FormLabel component="legend">In Stock</FormLabel>
+              <RadioGroup
+                className="inStock"
+                aria-label="In Stock"
+                name="instock"
+                value={1}
+              >
+                <FormControlLabel value="No" control={<Radio />} label="No" />
+                <FormControlLabel value="yes" control={<Radio />} label="yes" />
+              </RadioGroup>
+            </DialogContent>
+          </div>
+
           <DialogActions>
             <Button onClick={this.handleClose} color="primary">
               Cancel
